6fe845ea69ef668841ff181f7e57bd16c977ed63
Ryan Radtke <110181914+Ryanforce08@users.noreply.github.com>
2024-05-28 12:20:40 -0700

    it makes "kkkrkrkkrkrkr" sounds

---
+++ src/drivetrain.py
@@ -0,0 +1,82 @@
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
+
+import math
+import navx
+import wpimath.geometry
+import wpimath.kinematics
+import swervemodule
+import phoenix6
+kMaxSpeed = 3.0  # 3 meters per second
+kMaxAngularSpeed = math.pi  # 1/2 rotation per second
+
+
+class Drivetrain:
+    """
+    Represents a swerve drive style drivetrain.
+    """
+
+    def __init__(self) -> None:
+        # change these idk
+        self.frontLeftLocation = wpimath.geometry.Translation2d(0.381, 0.381)
+        self.frontRightLocation = wpimath.geometry.Translation2d(0.381, -0.381)
+        self.backLeftLocation = wpimath.geometry.Translation2d(-0.381, 0.381)
+        self.backRightLocation = wpimath.geometry.Translation2d(-0.381, -0.381)
+
+        self.frontLeft = swervemodule.SwerveModule(11, 12, 13)
+        self.frontRight = swervemodule.SwerveModule(21, 22, 23)
+        self.backLeft = swervemodule.SwerveModule(31, 32, 33)
+        self.backRight = swervemodule.SwerveModule(41, 42, 43)
+
+        self.gyro = navx.AHRS.create_spi()
+
+        self.kinematics = wpimath.kinematics.SwerveDrive4Kinematics(
+            self.frontLeftLocation,
+            self.frontRightLocation,
+            self.backLeftLocation,
+            self.backRightLocation,
+        )
+
+        self.gyro.reset()
+
+    
+
+
+    def drive(
+        self,
+        xSpeed: float,
+        ySpeed: float,
+        rot: float,
+        fieldRelative: bool,
+        periodSeconds: float,
+    ) -> None:
+        """
+        Method to drive the robot using joystick info.
+        :param xSpeed: Speed of the robot in the x direction (forward).
+        :param ySpeed: Speed of the robot in the y direction (sideways).
+        :param rot: Angular rate of the robot.
+        :param fieldRelative: Whether the provided x and y speeds are relative to the field.
+        :param periodSeconds: Time
+        """
+        swerveModuleStates = self.kinematics.toSwerveModuleStates(
+            wpimath.kinematics.ChassisSpeeds.discretize(
+                (
+                    wpimath.kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(
+                        xSpeed, ySpeed, rot, self.gyro.getRotation2d()
+                    )
+                    if fieldRelative
+                    else wpimath.kinematics.ChassisSpeeds(xSpeed, ySpeed, rot)
+                ),
+                periodSeconds,
+            )
+        )
+        wpimath.kinematics.SwerveDrive4Kinematics.desaturateWheelSpeeds(
+            swerveModuleStates, kMaxSpeed
+        )
+        self.frontLeft.setDesiredState(swerveModuleStates[0])
+        self.frontRight.setDesiredState(swerveModuleStates[1])
+        self.backLeft.setDesiredState(swerveModuleStates[2])
+        self.backRight.setDesiredState(swerveModuleStates[3])
---
+++ src/swervemodule.py
@@ -0,0 +1,176 @@
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
+
+import math
+import wpilib
+from wpilib.interfaces import MotorController
+import wpimath.kinematics
+import wpimath.geometry
+import wpimath.controller
+import wpimath.trajectory
+import phoenix6
+
+kWheelRadius = 0.0508
+kModuleMaxAngularVelocity = math.pi
+kModuleMaxAngularAcceleration = math.tau
+
+class WPI_TalonFX(phoenix6.hardware.TalonFX, MotorController):
+    """Wrapper for the phoenix6 TalonFX that implements
+    the wpilib MotorController interface, making it possible
+    to use TalonFX controllers in, for example, MotorControllerGroup
+    and DifferentialDrive
+    """
+
+    def __init__(self, device_id: int, canbus: str = "", enable_foc: bool = False):
+        phoenix6.hardware.TalonFX.__init__(self, device_id, canbus=canbus)
+        MotorController.__init__(self)
+        self.config = phoenix6.configs.TalonFXConfiguration()
+        self.duty_cycle_out = phoenix6.controls.DutyCycleOut(0, enable_foc=enable_foc)
+        self.voltage_out = phoenix6.controls.VoltageOut(0, enable_foc=enable_foc)
+        self.is_disabled = False
+
+    def disable(self):
+        self.stopMotor()
+        self.is_disabled = True
+
+    def get(self) -> float:
+        return self.duty_cycle_out.output
+
+    def getInverted(self) -> bool:
+        return (
+            self.config.motor_output.inverted
+            == phoenix6.signals.InvertedValue.COUNTER_CLOCKWISE_POSITIVE
+        )
+
+    def set(self, speed: float):
+        if not self.is_disabled:
+            self.duty_cycle_out.output = speed
+            self.set_control(self.duty_cycle_out)
+
+    def setIdleMode(self, mode: phoenix6.signals.NeutralModeValue):
+        """Set the idle mode setting
+
+        Arguments:
+        mode -- Idle mode (coast or brake)
+        """
+        self.config.motor_output.neutral_mode = mode
+        self.configurator.apply(self.config)
+
+    def setInverted(self, isInverted: bool):
+        if isInverted:
+            self.config.motor_output.inverted = phoenix6.signals.InvertedValue.CLOCKWISE_POSITIVE
+        else:
+            self.config.motor_output.inverted = phoenix6.signals.InvertedValue.COUNTER_CLOCKWISE_POSITIVE
+        self.configurator.apply(self.config)
+
+    def setVoltage(self, volts: float):
+        if not self.is_disabled:
+            self.voltage_out.output = volts
+            self.set_control(self.voltage_out)
+
+    def stopMotor(self):
+        self.set(0)
+
+class SwerveModule:
+    def __init__(
+        self,
+        driveMotorChannel: int,
+        turningMotorChannel: int,
+        turningEncoderChannel: int,
+    ) -> None:
+        """Constructs a SwerveModule with a drive motor, turning motor, drive encoder and turning encoder.
+
+        :param driveMotorChannel:      PWM output for the drive motor.
+        :param turningMotorChannel:    PWM output for the turning motor.
+        :param driveEncoderChannelA:   DIO input for the drive encoder channel A
+        :param driveEncoderChannelB:   DIO input for the drive encoder channel B
+        :param turningEncoderChannelA: DIO input for the turning encoder channel A
+        :param turningEncoderChannelB: DIO input for the turning encoder channel B
+        """
+        self.driveMotor = WPI_TalonFX(driveMotorChannel)
+        self.turningMotor = WPI_TalonFX(turningMotorChannel)
+        self.turningEncoder = phoenix6.hardware.CANcoder(turningEncoderChannel)
+
+        # Gains are for example purposes only - must be determined for your own robot!
+        self.drivePIDController = wpimath.controller.PIDController(1, 0, 0)
+
+        # Gains are for example purposes only - must be determined for your own robot!
+        self.turningPIDController = wpimath.controller.ProfiledPIDController(
+            1,
+            0,
+            0,
+            wpimath.trajectory.TrapezoidProfile.Constraints(
+                kModuleMaxAngularVelocity,
+                kModuleMaxAngularAcceleration,
+            ),
+        )
+
+        # Gains are for example purposes only - must be determined for your own robot!
+        self.driveFeedforward = wpimath.controller.SimpleMotorFeedforwardMeters(1, 3)
+        self.turnFeedforward = wpimath.controller.SimpleMotorFeedforwardMeters(1, 0.5)
+
+        # Limit the PID Controller's input range between -pi and pi and set the input
+        # to be continuous.
+        self.turningPIDController.enableContinuousInput(-math.pi, math.pi)
+
+    def getState(self) -> wpimath.kinematics.SwerveModuleState:
+        """Returns the current state of the module.
+
+        :returns: The current state of the module.
+        """
+        return wpimath.kinematics.SwerveModuleState(
+            self.driveMotor.get_velocity().value,
+            wpimath.geometry.Rotation2d(self.turningEncoder.get_position().value * 2 * math.pi),
+        )
+
+    def getPosition(self) -> wpimath.kinematics.SwerveModulePosition:
+        """Returns the current position of the module.
+
+        :returns: The current position of the module.
+        """
+        return wpimath.kinematics.SwerveModulePosition(
+            self.driveMotor.get_velocity().value,
+            wpimath.geometry.Rotation2d(self.turningEncoder.get_position().value * 2 * math.pi),
+        )
+
+    def setDesiredState(
+        self, desiredState: wpimath.kinematics.SwerveModuleState
+    ) -> None:
+        """Sets the desired state for the module.
+
+        :param desiredState: Desired state with speed and angle.
+        """
+
+        encoderRotation = wpimath.geometry.Rotation2d(self.turningEncoder.get_position().value * 2 * math.pi)
+
+        # Optimize the reference state to avoid spinning further than 90 degrees
+        state = wpimath.kinematics.SwerveModuleState.optimize(
+            desiredState, encoderRotation
+        )
+
+        # Scale speed by cosine of angle error. This scales down movement perpendicular to the desired
+        # direction of travel that can occur when modules change directions. This results in smoother
+        # driving.
+        state.speed *= (state.angle - encoderRotation).cos()
+
+        # Calculate the drive output from the drive PID controller.
+        driveOutput = self.drivePIDController.calculate(
+            self.driveMotor.get_velocity().value, state.speed
+        )
+
+        driveFeedforward = self.driveFeedforward.calculate(state.speed)
+
+        # Calculate the turning motor output from the turning PID controller.
+        turnOutput = self.turningPIDController.calculate(
+            self.turningEncoder.get_position().value * 2 * math.pi, state.angle.radians()
+        )
+
+        turnFeedforward = self.turnFeedforward.calculate(
+            self.turningPIDController.getSetpoint().velocity
+        )
+
+        self.driveMotor.setVoltage(driveOutput + driveFeedforward)
+        self.turningMotor.setVoltage(turnOutput + turnFeedforward)
